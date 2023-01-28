0f506b6dbc3e8cd0dc34faf17be4522437860b2d
Lucas Mickelsson <lucas.mickelsson@ha.ax>
2023-01-28 12:26:47 +0200

    added drawing application

--- plantsVsZombies/src/main/java/ax/ha/it/oo2/game/plantsvszombies/BucketZombie.java
+++
@@ -1,7 +0,0 @@
-package ax.ha.it.oo2.game.plantsvszombies;
-
-public class BucketZombie extends Zombie {
-    public BucketZombie(int x, int y) {
-        super(3, 1, x, y, 30, "/bucketheadzombie.gif", 68, 118);
-    }
-}
--- plantsVsZombies/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module ax.ha.it.oo2.game.plantsvszombies {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.media;
-
-
-    opens ax.ha.it.oo2.game.plantsvszombies to javafx.fxml;
-    exports ax.ha.it.oo2.game.plantsvszombies;
-}
