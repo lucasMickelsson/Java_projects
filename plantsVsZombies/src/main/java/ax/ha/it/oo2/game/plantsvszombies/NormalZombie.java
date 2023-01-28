9c9ddfcb95d564136f649019a9c7bb7556fada4c
Lucas Mickelsson <lucas.mickelsson@ha.ax>
2023-01-28 12:36:20 +0200

    added gui

--- plantsVsZombies/src/main/java/ax/ha/it/oo2/game/plantsvszombies/NormalZombie.java
+++
@@ -1,7 +0,0 @@
-package ax.ha.it.oo2.game.plantsvszombies;
-
-public class NormalZombie extends Zombie {
-    public NormalZombie(int x, int y) {
-        super(1, 1, x, y, 15, "/normalzombie.gif", 68, 118);
-    }
-}
